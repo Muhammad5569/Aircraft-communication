d2a9e3e5ba9272ebdf422bec889d29cc8756a295
Muhammad5569 <777muhammad5569@gmail.com>
2024-12-23 17:26:03 +0500

    atc-manager model

---
+++ src/models/atc-manager.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const ATCManagerSchema = new mongoose.Schema({
+    name:{
+            type: String,
+            required: true,
+        },
+        age:{
+            type: Number,
+            required: true,
+            validate(value){
+                if(value < 0){
+                    throw new Error('Age must be positive number')
+                }
+            }
+        },
+        role:{
+            type: String,
+            required: true,
+        },
+        airportName:{
+            type: String,
+            required: true,
+        },
+        airportAddress:{
+            type: String,
+            required: true,
+        },
+        phone:{
+            type: Number,
+            required: true,
+            unique: true,
+        },
+        email:{
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+            validate(value){
+                if(!validator.isEmail(value)){
+                    throw new Error('Email is invalid')
+                }
+            }
+        },
+        login:{
+            type: String,
+            required: true,
+            trim: true,
+        },
+        password:{
+            type: String,
+            required: true,
+            minlength: 7,
+        }
+    
+})
+
+const ATCManager = mongoose.model('ATCManager', ATCManagerSchema)
+module.exports = ATCManager
