68cbad256b93d8f5febcf3507ab434f6c638d477
Muhammad5569 <777muhammad5569@gmail.com>
2024-12-24 23:52:33 +0500

    fixded

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const routerPilot = require('./routers/pilots')
+const swaggerUi = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc')
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT | 3000
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Aircraft API',
+            version: '1.0.0',
+            description: 'Aircraft Communication API Documentation'
+        },
+        servers: [
+            {
+                url: `http://localhost:${port}/api-doc`
+            }
+        ]
+    },
+    apis: ['./src/routers/*.js']
+}
+
+const specs = swaggerJsDoc(options)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
 
 app.use(express.json()) 
 app.use(routerPilot)
